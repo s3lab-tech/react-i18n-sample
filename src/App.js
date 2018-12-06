b690c854fc1c5d9c20865e4a49b4124af3e990d0
Masayuki Matsuo <masa.y.m@gmail.com>
2018-12-07 01:13:06 +0900

    react i18n sample

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import JP from './img/jp.png'
+import US from './img/us.png'
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      lang: 'ja',
+      dic: require("./locales/ja/translation.json")
+    };
+  }
+
+  changeLang = (lang) => {
+    this.setState({
+      lang: lang,
+      dic: require("./locales/" + lang + "/translation.json")
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
@@ -19,6 +38,13 @@ class App extends Component {
           >
             Learn React
           </a>
+
+          <span>{this.state.dic["title"]}</span>
+
+          <a href="#" onClick={() => this.changeLang("ja")}><img src={JP} /></a><span>JP</span>
+
+          <a href="#" onClick={() => this.changeLang("en")}><img src={US} /></a><span>US</span>
+
         </header>
       </div>
     );
